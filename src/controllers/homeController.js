ca895d06213e05676fe44d4ded07ca3f8010a254
tranhoang <mailtranhoang@gmail.com>
2023-05-03 15:31:54 +0700

    connect database Render Data in View video 21

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,16 @@
+import db from '../models/index'
+let getHomePage = async (req, res) => {
+    try{
+        let data= await db.User.findAll();
+        console.log(data)
+        return res.render('homepage.ejs',{
+            data: JSON.stringify(data)
+        });
+    }catch(e){
+        console.log(e)
+    }
 
-let getHomePage = (req, res) => {
-    return res.render('homepage.ejs');
+
 }
 
 let getAboutPage = (req, res) => {
