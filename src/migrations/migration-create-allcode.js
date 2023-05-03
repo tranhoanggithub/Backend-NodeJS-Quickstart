1e939de95d02736cdc8f5a8ecf4d0eb3c5b22b42
tranhoang <mailtranhoang@gmail.com>
2023-05-03 23:18:16 +0700

    tao model allcode

---
+++ src/migrations/migration-create-allcode.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    
+    await queryInterface.createTable('allcode', {
+      // id:DataTypes.STRING,
+      // email: DataTypes.STRING,
+      // firstName: DataTypes.STRING,
+      // lastName: DataTypes.STRING,
+      // address :DataTypes.STRING,
+      // gender:DataTypes.BOOLEAN,
+      // roleid:DataTypes.STRING,
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      key: {
+        type: Sequelize.STRING
+      },
+      type: {
+        type: Sequelize.STRING
+      },
+      valueEn: {
+        type: Sequelize.STRING
+      },
+      valueVi: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('allcode');
+  }
+};
