0a558d9a718149cd5321f7f44ac4f32e6325b8a3
tranhoang <mailtranhoang@gmail.com>
2023-12-30 23:32:32 +0700

    Import Data tu EXCEL va viet API GET ALL CODE

--- src/route/web.js
+++ src/route/web.js
@@ -20,11 +20,13 @@ let initWebRoutes = (app) => {
     router.get('/api/get-all-users', userController.handleGetAllUsers);
     router.post('/api/create-new-user', userController.handleCreateNewUser);
     router.put('/api/edit-user', userController.handleEditUser);
-    router.delete('/api/delete-user', userController.handleDeleteUser);
+    router.delete('/api/delete-user', userController.handleDeleteUser); //restAPI
+
+    router.get('/allcode', userController.getAllCode);
 
 
 
     return app.use("/", router);
 }
 
-module.exports = initWebRoutes; 
+module.exports = initWebRoutes;
