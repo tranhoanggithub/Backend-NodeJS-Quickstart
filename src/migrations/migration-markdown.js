b94051962c19e3b0413b0a3dbb3430c009ef585f
tranhoang <mailtranhoang@gmail.com>
2024-02-01 12:36:51 +0700

    #65 Cai Dat Va Su Dung Markdown Editor-Host Anh  Free  Voi Github

---
+++ src/migrations/migration-markdown.js
@@ -0,0 +1,61 @@
+'use strict';
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        // statusId:DataTypes.STRING,
+        // doctorId:DataTypes.INTEGER,
+        // patienId:DataTypes.INTEGER,
+        // date:DataTypes.DATE,
+        // timeType:DataTypes.STRING,
+
+        await queryInterface.createTable('markdowns', {
+            // id:DataTypes.STRING,
+            // email: DataTypes.STRING,
+            // firstName: DataTypes.STRING,
+            // lastName: DataTypes.STRING,
+            // address :DataTypes.STRING,
+            // gender:DataTypes.BOOLEAN,
+            // roleid:DataTypes.STRING,
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            contentHTML: {
+                allowNull: false,
+                type: Sequelize.TEXT('long')
+            },
+            contentMarkdown: {
+                allowNull: false,
+                type: Sequelize.TEXT('long')
+            },
+            description: {
+                allowNull: true,
+                type: Sequelize.TEXT('long')
+            },
+            doctorId: {
+                allowNull: false,
+                type: Sequelize.INTEGER
+            },
+            specialtyId: {
+                allowNull: false,
+                type: Sequelize.INTEGER
+            },
+            clinicId: {
+                allowNull: false,
+                type: Sequelize.INTEGER
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('markdowns');
+    }
+};
