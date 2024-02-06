3efe29e42731b1895f8292347c7fe8ad6c52791d
tranhoang <mailtranhoang@gmail.com>
2024-02-06 15:06:53 +0700

    #69 Tao Giao Dien Trang Xem Chi Tiet Thong Tin Bac Si Phia Client the end video

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -91,7 +91,7 @@ let getDetailDoctorById = (inputId) => {
                         id: inputId
                     },
                     attributes: {
-                        exclude: ['password', 'image']
+                        exclude: ['password']
                     },
                     include: [
                         {
@@ -100,9 +100,15 @@ let getDetailDoctorById = (inputId) => {
                         },
                         { model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi'] },
                     ],
-                    raw: true,
+                    raw: false,
                     nest: true
                 })
+                if (data && data.image) {
+                    data.image = new Buffer(data.image, 'base64').toString('binary');
+
+                }
+
+                if (!data) data = {};
                 resolve({
                     errCode: 0,
                     data: data
